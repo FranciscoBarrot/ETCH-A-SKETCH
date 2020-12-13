6847b1957a7f6833d397678c7bf2800789e2acde
Barrot Francisco <franciscobarrot98@gmail.com>
2020-12-12 21:26:17 -0300

    fourth commit, me salio

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/* FUNCTIONS */
+function changeColor(e){
+    e.target.classList.add("pixelated")
+}
+
+
+
 /* GRID SQUARES */
 const gridContainer = document.querySelector(".container")
 
@@ -6,6 +13,6 @@ const gridContainer = document.querySelector(".container")
 for (i = 0; i < 256; i++){
     const gridSquare = document.createElement("div")
     gridSquare.classList.add("gridSquare")
-    gridSquare.addEventListener("mouseover", )
+    gridSquare.addEventListener("mouseover", changeColor)
     gridContainer.appendChild(gridSquare)
 }
