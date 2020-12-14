caa610f523b4244f66b00ddd0b46cbca093962a7
Barrot Francisco <franciscobarrot98@gmail.com>
2020-12-14 11:50:31 -0300

    sixth commit, clear button done

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
-/* FUNCTIONS */
-function changeColor(e){
-    e.target.classList.add("pixelated")
+function iterateSquares(squaresPerSide, process){
+    for (i = 0; i < (squaresPerSide ** 2); i ++){
+        process()
+    }
 }
 
 
 
 /* GRID SQUARES */
-const gridContainer = document.querySelector(".container")
+function changeColor(e){
+    e.target.classList.add("pixelated")
+}
 
-
-
-for (i = 0; i < 256; i++){
+function makeSquare(){
     const gridSquare = document.createElement("div")
     gridSquare.classList.add("gridSquare")
     gridSquare.addEventListener("mouseover", changeColor)
     gridContainer.appendChild(gridSquare)
 }
+
+
+
+const gridContainer = document.querySelector(".container")
+iterateSquares(16,makeSquare)
+
+
+/* for (i = 0; i < 256; i++){
+    const gridSquare = document.createElement("div")
+    gridSquare.classList.add("gridSquare")
+    gridSquare.addEventListener("mouseover", changeColor)
+    gridContainer.appendChild(gridSquare)
+} */
+
+
+/* BUTTON */
+function clearAndAlert(){
+    const divs = document.querySelectorAll(".gridSquare")
+    divs.forEach( (gridSquare) => {
+        gridContainer.removeChild(gridSquare)
+    })
+
+
+    result = prompt("How many squares per side do you want?")
+    while ((result > 100) || (result < 1)) {
+        result = prompt("Out of range! How many squares per side do you want?")
+    }
+    gridContainer.setAttribute("style", `grid-template-columns: repeat(${result}, 1fr); grid-template-rows: repeat(${result}, 1fr)`)
+    iterateSquares(result, makeSquare)
+}
+
+
+
+const btn = document.querySelector(".clearButton")
+btn.addEventListener("click", clearAndAlert)
